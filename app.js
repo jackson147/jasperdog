d89487875409289d5e0f283351942f44d7a65245
Richard Jackson <jaswahhihi@gmail.com>
2020-04-06 10:06:36 +0200

    Add a generic catch all for any errors, this needs to be improved with some actual error handling

--- app.js
+++ app.js
@@ -14,4 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 require('./routes.js')(app, urlsArray);
 app.listen(port, () => console.log(`JasperDog is listening on port ${port}!`))
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('You are trying to use this bot outside of Slack. Jasper is sad dog...')
+})
 
