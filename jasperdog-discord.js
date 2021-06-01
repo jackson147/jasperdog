c65f92891927e53ff83f0a891d9de2b638908c70
jackson147 <developer.jacksondroid@gmail.com>
2021-06-01 14:28:45 +0100

    Revert "multiple jaspers attempt 2"

--- jasperdog-discord.js
+++ jasperdog-discord.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const jaspertools = require('./jasper-tools');
+const fs = require('fs')
+const jaspertools = require('./jasper-tools')
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require("discord.js")
+const client = new Discord.Client()
 let discordSecret = fs.readFileSync("/run/secrets/discord-secret").toString('utf8').trim();
 
 module.exports = function(){
@@ -11,18 +11,13 @@ module.exports = function(){
     })
     client.on("message", msg => {
         let messageContent = msg.content.toLowerCase()
-        if (messageContent.match("jasper").length > 0) {
+        if (messageContent.startsWith("jasper")) {
             if(messageContent.includes("carrot")){
                 msg.reply("https://i2-prod.birminghammail.co.uk/incoming/article14143650.ece/ALTERNATES/s810/Jasper-face-crop.jpg")
             }else{
-                //To stop someone taking the piss
-                const numOfJaspers = messageContent.match("/jasper/gm").length <= 5 ? messageContent.match("jasper").length : 5;
-
-                for (let i = 0; i < numOfJaspers; i++) {
-                    msg.reply(jaspertools.getRandomImageUrl(true))
-                }
+                msg.reply(jaspertools.getRandomImageUrl(true))
             }
         }
-    });
+    })
     client.login(discordSecret)
-};
+}
