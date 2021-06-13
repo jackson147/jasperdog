b0ceb68679967ff2b808660ef7aa17475502cac4
Dodplas <81987640+Dodplas@users.noreply.github.com>
2021-06-13 17:45:08 +0100

    matt multiple jasper work

--- discord/jasperdog-discord-service.js
+++ discord/jasperdog-discord-service.js
@@ -1,11 +1,23 @@
 const jaspertools = require('../jasper-tools')
 
 const getImage = function(messageContent){
-    if (messageContent.startsWith("jasper")) {
+    console.log(messageContent.match("/jasper/gm"));
+    console.log(messageContent.regexp("/jasper/gm"));
+
+    if (messageContent.match("/jasper/gm").length > 0) {
         if(messageContent.includes("carrot")){
             return "https://i2-prod.birminghammail.co.uk/incoming/article14143650.ece/ALTERNATES/s810/Jasper-face-crop.jpg"
         }else{
-           return jaspertools.getRandomImageUrl(true)
+            // return jaspertools.getRandomImageUrl(true)
+            let response;
+
+
+            //To stop someone taking the piss
+            const numOfJaspers = messageContent.match("/jasper/gm").length <= 5 ? messageContent.match("/jasper/gm").length : 5;
+
+            for (let i = 0; i < numOfJaspers; i++) {
+                response =+ " " + jaspertools.getRandomImageUrl(true);
+            }
         }
     }
     return undefined
