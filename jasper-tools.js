bcaee84f1686ab39a1c360d55dfdb167e123d379
jackson147 <jaswahhihi@gmail.com>
2021-10-28 11:21:25 +0100

    Inital setup of reading images from minio working

--- jasper-tools.js
+++ jasper-tools.js
@@ -1,11 +1,41 @@
 const fs = require('fs')
 const allUrls = fs.readFileSync("./urls.txt").toString('utf8');
 const urlsArray = allUrls.split("\n");
+const config = require('./config.json')
 
-module.exports.getRandomImageUrl = function(stripMedium){
-    let url = urlsArray[Math.floor(Math.random() * urlsArray.length)];
-    if(stripMedium){
-        url = url.replace("m.jpg", ".jpg");
-    }
+var Minio = require('minio')
+var minioClient = new Minio.Client(config.minio);
+
+let imageArray = []
+
+const refreshImages = function(){
+    console.log("REFRESHING IMAGE CACHE")
+    var stream = minioClient.listObjectsV2('images','', true,'')
+    stream.on('data', function (obj) {
+        let fileName = obj.name;
+        if (fileName.endsWith(".jpg")) {
+            if(!imageArray.includes(fileName)){
+                imageArray.push(fileName);
+            }
+        }
+    })
+}
+
+const getRandomImageUrl = function(stripMedium){
+    var stream = minioClient.listObjectsV2('images','', true,'')
+    stream.on('data', function (obj) {
+        let fileName = obj.name;
+        if (fileName.endsWith(".jpg")) {
+            imageArray.push(fileName);
+        }
+    })
+    stream.on('error', function(err) { console.log(err) } )
+
+    let url = imageArray[Math.floor(Math.random() * imageArray.length)];
     return url;
+}
+
+module.exports = {
+    'getRandomImageUrl': getRandomImageUrl,
+    'refreshImages' : refreshImages
 }
